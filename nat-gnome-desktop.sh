5ddf7d80898f4cd3d8a0eb523261a8afe11d20ce
jonathanctech <jonathancamarce@outlook.com>
2024-02-02 05:56:56 -0600

    updated gitl

--- nat-gnome-desktop.sh
+++ nat-gnome-desktop.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# update
 sudo apt update
+sudo apt install nala 
 
-#install utils
-sudo apt install nala vim build-essential default-jdk curl wget neofetch ufw flatpak nextcloud-desktop yubioath-desktop vlc iperf3 neovim htop cifs-utils -y
+# install utils
+sudo nala install vim build-essential default-jdk curl wget neofetch ufw flatpak nextcloud-desktop yubioath-desktop vlc iperf3 neovim htop cifs-utils -y
 
-#add alias to bashrc
+
+# add alias to bashrc
 cp .bash_aliases ~
 cp .tmux.conf ~
 source .bashrc
 
-#upgrade
+# upgrade
 sudo nala upgrade -y
 
-#brave
+# brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -23,13 +26,13 @@ sudo nala update
 sudo nala install brave-browser -y
 
 
-#unintall bloat w/ nala
+# unintall bloat w/ nala
 sudo nala purge gnome-games kde-baseapps libreoffice* firefox-esr -y
 
-#flathub repo
+# flathub repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-#flatpak apps
+# flatpak apps
 flatpak install com.discordapp.Discord com.yubico.yubioath nl.hjdskes.gcolor3 org.libreoffice.LibreOffice -y
 
 # gnome-desktop
@@ -37,5 +40,6 @@ flatpak install com.discordapp.Discord com.yubico.yubioath nl.hjdskes.gcolor3 or
 # flatpak install com.mattjakeman.ExtensionManager 
 
 # kde
+# sudo nala install plasma-discover-backend-flatpak 
 
 #xfce
