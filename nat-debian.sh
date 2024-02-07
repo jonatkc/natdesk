1689ca03250321c9e4d6be502d3b7f2e7a9e2333
jonathanctech <jonathancamarce@outlook.com>
2024-02-06 20:59:38 -0600

    updated gitl

--- nat-debian.sh
+++ nat-debian.sh
@@ -7,7 +7,7 @@ sudo apt install nala
 ########## Minimal install ##########
 
 # install utils
-sudo nala install vim curl wget neofetch ufw flatpak vlc iperf3 neovim htop cifs-utils -y
+sudo nala install vim curl wget neofetch ufw flatpak vlc iperf3 neovim htop btop cifs-utils -y
 # non essential 
 # sudo nala install nextcloud-desktop yubioath-desktop
 
