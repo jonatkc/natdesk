d0446ff2f4d9134502448fb053cfb4b92eb388df
jonathan <jonathancamarce@outlook.com>
2024-02-19 23:50:01 -0600

    update

--- nat-fedora.sh
+++ nat-fedora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo dnf install gcc-c++ java-devel neofetch neovim clang gnome-shell-extension-pop-shell tmux curl htop btop vlc -y
+sudo dnf install gcc-c++ chromium-browser java-devel neofetch neovim clang gnome-shell-extension-pop-shell tmux curl htop btop vlc -y
 sudo dnf upgrade -y
 sudo dnf remove firefox -y
 flatpak install discord extensionmanager nextcloud firefox steam -y
