5d509b3037229c83275275203b2b106883a8faab
Jonathan <jonathancamarce@outlook.com>
2024-02-21 13:34:57 -0600

    update

--- nat-fedora.sh
+++ nat-fedora.sh
@@ -9,3 +9,6 @@ sudo dnf upgrade -y
 #### optional ####
 # sudo dnf remove firefox -y
 # flatpak install discord extensionmanager nextcloud firefox steam -y
+
+#### Brave ####
+sudo dnf install dnf-plugins-core ; sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo ; sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc ; sudo dnf install brave-browser -y
