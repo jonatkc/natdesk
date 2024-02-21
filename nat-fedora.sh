7b9cb8c5877649ad62fa0749aa0a0a37dd672c75
Jonathan <jonathancamarce@outlook.com>
2024-02-21 15:44:13 -0600

    update

--- nat-fedora.sh
+++ nat-fedora.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #### essentials ####
-sudo dnf install zoxide chromium-browser neofetch neovim gnome-shell-extension-pop-shell tmux curl htop btop vlc -y
+sudo dnf install zoxide chromium.x86_64 neofetch neovim gnome-shell-extension-pop-shell tmux curl htop btop vlc -y
 sudo dnf upgrade -y
 
 #### developer settings ####
@@ -11,4 +11,4 @@ sudo dnf upgrade -y
 # flatpak install discord extensionmanager nextcloud firefox steam -y
 
 #### Brave ####
-sudo dnf install dnf-plugins-core ; sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo ; sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc ; sudo dnf install brave-browser -y
+# sudo dnf install dnf-plugins-core ; sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo ; sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc ; sudo dnf install brave-browser -y
