d7745d8a1aecba68a130b07bc2d30fabae99e423
Jonathan Camarce <105486415+jonatkc@users.noreply.github.com>
2024-08-28 08:59:47 -0500

    Add files via upload
    windows script

---
+++ nat-win.bat
@@ -0,0 +1,6 @@
+// install winget 
+// Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+
+
+winget install aristocratos.btop4win Neovim.Neovim Nextcloud.NextcloudDesktop Doist.Todoist Brave.Brave Valve.Steam Discord.Discord XP89DCGQ3K6VLD Fastfetch-cli.Fastfetch Proton.ProtonVPN VideoLAN.VLC Microsoft.WindowsTerminal glazewm zoxide
+
