e5a040d3afd600876dd172092be1393d6f7aa6ae
Jonathan Camarce <jonathancamarce@outlook.com>
2025-01-27 23:38:27 -0600

    dotfiles.d

--- dotfiles.f/.config/nvim/lua/plugins/colorMyTerminal.lua
+++ dotfiles.f/.config/nvim/lua/plugins/colorMyTerminal.lua
@@ -11,16 +11,16 @@
 --vim.cmd[[colorscheme nord]]
 
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme catppuccin]])
-      vim.api.nvim_set_hl(1,"Normal", {bg = "none"})
-      vim.api.nvim_set_hl(1,"NormalFloat", {bg = "none"})
-    end,
-  }
+	-- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme catppuccin]])
+			vim.api.nvim_set_hl(1, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(1, "NormalFloat", { bg = "none" })
+		end,
+	},
 }
