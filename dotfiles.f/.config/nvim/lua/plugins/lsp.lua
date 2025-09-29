147b3d87566354fb1aec8bf59c9ac344f010e679
Jonathan Camarce <jonathancamarce@outlook.com>
2025-09-29 11:55:26 -0500

    hypr conf

--- dotfiles.f/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles.f/.config/nvim/lua/plugins/lsp.lua
@@ -207,7 +207,7 @@ return {
 			{
 				"<leader>f",
 				function()
-					require("conform").format({ async = true, lsp_fallback = true })
+					require("conform").format({ async = true, lsp_fallback = false })
 				end,
 				mode = "",
 				desc = "[F]ormat buffer",
@@ -215,16 +215,6 @@ return {
 		},
 		opts = {
 			notify_on_error = false,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				return {
-					timeout_ms = 500,
-					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-				}
-			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
 				c = { "clang-format" },
